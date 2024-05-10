dc12ee1d1d25a5b245b242c9a63eb3503ed123cb
Mahabub <mahabubx7@gmail.com>
2024-05-10 19:35:19 +0600

    wip: added initial setup

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, 'always', 100],
+    'scope-case': [0, 'always', 'pascal-case'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'wip',
+      ],
+    ],
+  },
+};
